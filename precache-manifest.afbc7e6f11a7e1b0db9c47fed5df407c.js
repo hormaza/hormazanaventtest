c84b006c0020578ec7048592ceb853c9d6fe1746
Matias Hormaza <hormazam@globalhitss.com>
2020-08-28 16:02:20 -0300

    Updates

---
+++ precache-manifest.afbc7e6f11a7e1b0db9c47fed5df407c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2fdb31ddfff2ce175d13990bb10c339",
+    "url": "/hormazanaventtest/index.html"
+  },
+  {
+    "revision": "38cc5897d03df890ba8a",
+    "url": "/hormazanaventtest/static/css/main.c6583b7b.chunk.css"
+  },
+  {
+    "revision": "bad6cc9b635ae8d481b5",
+    "url": "/hormazanaventtest/static/js/2.71ce39a2.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/hormazanaventtest/static/js/2.71ce39a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38cc5897d03df890ba8a",
+    "url": "/hormazanaventtest/static/js/main.38c2a182.chunk.js"
+  },
+  {
+    "revision": "36845c15bfc9c506a949",
+    "url": "/hormazanaventtest/static/js/runtime-main.e831f6fa.js"
+  }
+]);
