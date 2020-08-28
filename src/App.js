18bc6fca515309b47a3c18a71300fc3551f8d261
Matias Hormaza <hormazam@globalhitss.com>
2020-08-28 15:55:41 -0300

    add Filter Operation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,69 @@
-import React from 'react';
-import postingList from './services/postings'
+import React, { useState } from 'react';
+import styled from 'styled-components'
+// services
+import POSTINGS from './services/postings'
 // components
 import Article from './components/Article'
-import styled from 'styled-components'
+import Filter from './components/Filter'
 
 function App() {
+  const [postingList, setPostingList] = useState(POSTINGS)
+  const [operationType, setOperationType] = useState('Todos')
+
+  const directionHandler = (direction) => {
+    const newPostingList = POSTINGS.filter(post =>
+      post.posting_location.address.toLocaleLowerCase().indexOf(direction.toLocaleLowerCase()) != -1 ||
+      post.posting_location.zone.toLocaleLowerCase().indexOf(direction.toLocaleLowerCase()) != -1 ||
+      post.posting_location.city.toLocaleLowerCase().indexOf(direction.toLocaleLowerCase()) != -1
+    )
+    setPostingList(newPostingList)
+  }
+
+  const getOperationTypeId = type => type === 'Alquiler' ? 1 :
+    type === 'Comprar' ? 2 :
+      type === 'Temporal' ? 3 :
+        0
+
+  const operationTypeHandler = (type) => {
+    const newPostingList = POSTINGS.filter(post =>
+      post.operation_type.operation_type_id === getOperationTypeId(type) ||
+      getOperationTypeId(type) === 0)
+    setOperationType(type)
+    setPostingList(newPostingList)
+  }
+  const compareFunction = (curr, next) => curr.publication_plan === 'SUPERHIGHLIGHTED' ? -1 :
+    curr.publication_plan === 'HIGHLIGHTED' && next.publication_plan !== 'SUPERHIGHLIGHTED' ? -1 :
+      0
   return (
     <GlobalStyles>
-      <section>
-
+      <section id='filter'>
+        <Filter
+          operationType={operationType}
+          onChangeDirection={directionHandler}
+          onOperationTypeChange={operationTypeHandler} />
       </section>
-      <section>
+      <section id='results'>
         {
-          postingList.map((post) =>
-            <Article
-              title={post.title}
-              address={post.posting_location.address}
-              zone={post.posting_location.zone}
-              city={post.posting_location.city}
-              description={post.posting_description}
-              publishDate={post.publish_date}
-              picture={post.posting_picture}
-              price={post.posting_prices[0].price.amount}
-              currency={post.posting_prices[0].price.currency}
-              plan={post.publication_plan}
-              expenses={
-                post.posting_prices[0].expenses &&
-                post.posting_prices[0].expenses.amount
-              }
-            />
-          )
+          postingList
+            .sort(compareFunction)
+            .map((post) =>
+              <Article
+                title={post.title}
+                address={post.posting_location.address}
+                zone={post.posting_location.zone}
+                city={post.posting_location.city}
+                description={post.posting_description}
+                publishDate={post.publish_date}
+                picture={post.posting_picture}
+                price={post.posting_prices[0].price.amount}
+                currency={post.posting_prices[0].price.currency}
+                plan={post.publication_plan}
+                expenses={
+                  post.posting_prices[0].expenses &&
+                  post.posting_prices[0].expenses.amount
+                }
+              />
+            )
         }
       </section>
 
@@ -43,7 +77,16 @@ const GlobalStyles = styled.main`
   background-color: #f0f0f0;
   padding: 30px;
   display: flex;
-  flex-direction: column;
+  #filter{
+    flex-grow: 1;
+  }
+
+  #results{
+    flex-grow: 2;
+  }
+  section {
+    margin: 0 5px;
+  }
 `
 
 export default App;
